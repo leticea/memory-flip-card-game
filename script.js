7441b38027ec3bcff420bb7cfc55cc99af31866c
leticea <nascimento.leticeah@gmail.com>
2023-03-06 17:32:47 -0300

    js coding

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ let matchedCards = 0;
 let disableDeck = false;
 let isPlaying = false;
 let cardOne, cardTwo, timer;
+
+function initTimer() {
+  if (timeLeft <= 0) {
+    return clearInterval(timer);
+  }
+  timeLeft--;
+  timeTag.innerText = timeLeft;
+}
+
+function flipCard({target: clickedCard}) {
+  if (!isPlaying) {
+    isPlaying = true;
+    timer = setInterval(initTimer, 1000);
+  }
+  if (clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
+    flips++;
+    flipsTag.innerText = flips;
+    clickedCard.classList.add("flip");
+    if (!cardOne) {
+      return cardOne = clickedCard;
+    }
+    cardTwo = clickedCard;
+    disableDeck = true;
+    let cardOneIcon = cardOne.querySelector(".back-view i").classList.value;
+    cardTwoIcon = cardTwo.querySelector(".back-view i").classList.value;
+  }
+}
